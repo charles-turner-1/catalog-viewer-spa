8f6d62a4c3fd9e0957949f99b106c4ac637cc909
Charles Turner <charles.turner@anu.edu.au>
2025-11-14 08:48:28 +0800

    Wrong path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/interactive-catalog-spa/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/catalog-viewer-spa/' : '/',
   plugins: [vue()],
   server: {
     proxy: {
