4932e3e55c563ea223bb07f493697421e8ad96ed
Charles Turner <charles.turner@anu.edu.au>
2026-08-25 15:57:04 +0800

    Revert "Point the datalake fetch at the staging server with new parquet setup (#261)"
    This reverts commit fd3c4c68f3431ad8b2aedf1f50df5943e27c9463.

--- src/config/catalogEndpoints.ts
+++ src/config/catalogEndpoints.ts
@@ -2,9 +2,7 @@ const objectStoreBaseUrl =
   'https://object-store.rc.nectar.org.au/v1/AUTH_685340a8089a4923a71222ce93d5d323/access-nri-intake-catalog';
 
 export const trackingServicesBaseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://reporting-dev2.access-nri-store.cloud.edu.au/'
-    : 'http://127.0.0.1:8000/';
+  process.env.NODE_ENV === 'production' ? 'https://reporting.access-nri-store.cloud.edu.au/' : 'http://127.0.0.1:8000/';
 
 export const metacatUrl = `${objectStoreBaseUrl}/metacatalog.parquet`;
 
