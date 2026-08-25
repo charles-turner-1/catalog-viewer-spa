5e947e0b450fb77b61b3c897c43537c22d4612cd
Charles Turner <charles.turner@anu.edu.au>
2026-08-25 15:52:06 +0800

    Fix test

--- src/config/__tests__/catalogEndpoints.spec.ts
+++ src/config/__tests__/catalogEndpoints.spec.ts
@@ -44,7 +44,7 @@ describe('catalogEndpoints', () => {
     vi.resetModules();
 
     const { trackingServicesBaseUrl: prodUrl } = await import('../catalogEndpoints');
-    expect(prodUrl).toBe('https://reporting.access-nri-store.cloud.edu.au/');
+    expect(prodUrl).toBe('https://reporting-dev2.access-nri-store.cloud.edu.au/');
 
     vi.unstubAllEnvs();
     vi.resetModules();
