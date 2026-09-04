319bbc61c2c399cf618e4d3e83aa8312c30e428a
Charles Turner <52199577+charles-turner-1@users.noreply.github.com>
2026-09-04 13:56:08 +1000

    Revert "Add hive docs link  (#264)" (#270)
    This reverts commit 3d58f441efc68e0282dad45fa9f4ac2641e1a9d2.

--- src/components/__tests__/RequiredProjectsWarning.spec.ts
+++ src/components/__tests__/RequiredProjectsWarning.spec.ts
@@ -44,8 +44,8 @@ describe('RequiredProjectsWarning', () => {
     expect(wrapper.text()).toContain('dk92');
     expect(wrapper.text()).toContain('ab12');
     const projectSpans = wrapper.findAll('span').filter((span) => span.classes().includes('px-2'));
-    expect(projectSpans.length).toBe(3 + 1 + 1);
-    // Extra span for the conda warning now, and a temporary +1 for the Hive docs
+    expect(projectSpans.length).toBe(3 + 1);
+    // Extra span for the conda warning now
   });
 
   // Test that singular "project" text is used when only one project
